cd714b000c6f80beaa51714ee77e1b9ffb93705a
Matt Willcox <mattjwillcox@gmail.com>
2017-09-04 16:02:18 +0000

    Adds rspec

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,4 @@
+#Base controller for admin name space. Sets authorization to use admin section
 class Admin::BaseController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTHENTICATE_USER_ID'], password: ENV['AUTHENTICATE_PASSWORD']
 end
